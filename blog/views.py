7c018bba50f236e4d564b5cdfebc9db2a7cc9a5f
Ilian Dimitrov <iliandimitrov0@gmail.com>
2019-07-27 01:46:05 +0300

    Changes on the front-end nevigation bar

--- blog/views.py
+++ blog/views.py
@@ -23,19 +23,18 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'# <app(blog)>/<model(Post)_<viewtype(List)>.html
     context_object_name = 'posts'
+    ordering = ['-date_posted']
     paginate_by = 5
+
 class UserPostListView(ListView):
     model = Post
     template_name = 'blog/user_posts.html'# <app(blog)>/<model(Post)_<viewtype(List)>.html
     context_object_name = 'posts'
-    ordering = ['-date_posted']
     paginate_by = 5
 
     def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs.get('username'))
         return Post.objects.filter(author=user).order_by('-date_posted')
-
-
 
 class PostDetailView(DetailView):
     model = Post
