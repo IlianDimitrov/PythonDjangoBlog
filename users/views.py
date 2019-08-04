0942c169d42b72b3d1eec512edf883a43cdd7818
Ilian Dimitrov <iliandimitrov0@gmail.com>
2019-08-04 20:36:55 +0300

    Fixed a bug with the register model

--- users/views.py
+++ users/views.py
@@ -14,7 +14,9 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Your account has been created!')
-            return redirect('login')
+
+        return redirect('login')
+
 
     else:
         form = UserRegisterForm()
